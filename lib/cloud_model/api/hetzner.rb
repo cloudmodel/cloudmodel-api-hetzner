fe4c906bc3af3684286436ba911943e68ee5b7a4
Sven G. Brönstrup <broenstrup@spom.net>
2020-12-03 18:05:52 +0100

    Added missing request functions

--- lib/cloud_model/api/hetzner.rb
+++ lib/cloud_model/api/hetzner.rb
@@ -8,3 +8,47 @@ require "cloud_model/api/hetzner/monitoring"
 require "cloud_model/api/hetzner/rdns"
 require "cloud_model/api/hetzner/server"
 
+module CloudModel
+  module Api
+    module Hetzner
+      def self.send_request request
+        uri = URI.parse("https://robot-ws.your-server.de/")
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        request.basic_auth CloudModel.config.api.hetzner.user, CloudModel.config.api.hetzner.password
+
+        response = http.request request
+
+        if response.kind_of? Net::HTTPSuccess
+          begin
+            JSON.parse response.body
+          rescue
+            response.body
+          end
+        else
+          puts "error #{response.value}: #{response.body}"
+          false
+        end
+      end
+
+      def self.request_get action
+        uri = URI("https://robot-ws.your-server.de/#{action}")
+        send_request(Net::HTTP::Get.new uri)
+      end
+
+      def self.request_delete action
+        uri = URI("https://robot-ws.your-server.de/#{action}")
+        send_request Net::HTTP::Delete.new uri
+      end
+
+      def self.request_post action, data
+        uri = URI("https://robot-ws.your-server.de/#{action}")
+        request = Net::HTTP::Post.new uri
+        request.set_form_data data
+        send_request request
+      end
+    end
+  end
+end
